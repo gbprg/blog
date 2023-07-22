e9b0b0ec0ebf1e5ddf1e2d29dba57af66e113846
gbprg <duarte.gabrielgbd@gmail.com>
2023-07-22 17:17:47 -0300

    feat: add context

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,45 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { AuthProvider } from "./Context/AuthContext";
+
+import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 export default function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <>
       <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/Login" element={<Login />} />
-              <Route path="/Register" element={<Register />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
+        <AuthProvider value={{ user }}>
+          <BrowserRouter>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Register" element={<Register />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
       </div>
     </>
   );
