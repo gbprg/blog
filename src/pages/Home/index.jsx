99ce9774c8b6b58af58d461adcdfcfda2ff36bb8
Gabriel Duarte <duarte.gabrielgbd@gmail.com>
2023-07-29 17:13:48 -0300

    chore: hook fethdocuments e suas funcionalidades para o post na home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import PostDetail from "../../components/PostDetail";
+
 import styles from "./Home.module.css";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  const [query, setQuery] = useState("");
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className={styles.home}>
+        <h1>Veja os nossos posts mais recentes</h1>
+        <form action="" onSubmit={handleSubmit} className={styles.search_form}>
+          <input
+            type="text"
+            placeholder="Ou busque por tags..."
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="btn btn-dark">Pesquisar</button>
+        </form>
+        <div>
+          {loading && <p>Carregando...</p>}
+          {posts &&
+            posts.map((post) => <PostDetail key={post.id} post={post} />)}
+          {posts && posts.length === 0 && (
+            <div className={styles.noposts}>
+              <p>Nenhum post encontrado</p>
+              <Link to="/posts/create" className="btn">
+                Criar primeiro post
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
